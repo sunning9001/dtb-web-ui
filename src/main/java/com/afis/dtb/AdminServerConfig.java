813a77da4d36d23fef1a59659c487752548d7fba
sunning <sunning@auto-fis.com>
2018-05-15 17:27:57 +0800

    admin server 增加集群功能

---
+++ src/main/java/com/afis/dtb/AdminServerConfig.java
@@ -0,0 +1,22 @@
+package com.afis.dtb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.ListConfig;
+import com.hazelcast.config.MapConfig;
+
+@Configuration
+public class AdminServerConfig {
+
+	@Bean
+	public Config hazelcastConfig() {
+		return new Config().setProperty("hazelcast.jmx", "true")
+				.addMapConfig(new MapConfig("spring-boot-admin-application-store").setBackupCount(1)
+						.setEvictionPolicy(EvictionPolicy.NONE))
+				.addListConfig(new ListConfig("spring-boot-admin-event-store").setBackupCount(1).setMaxSize(1000));
+	}
+
+}
