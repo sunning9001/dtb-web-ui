b953504efa5e21d54bdd7e425a34c068eda422f2
sunning <sunning@auto-fis.com>
2018-06-07 10:43:49 +0800

    Revert "Revert "监控升级到2.0.0版本""
    This reverts commit 01a009841099ab594f4403187b78a79b7eca3a22.

--- src/main/java/com/afis/dtb/SpringBootAdminApplication.java
+++ src/main/java/com/afis/dtb/SpringBootAdminApplication.java
@@ -2,16 +2,34 @@ package com.afis.dtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-		
+
+	@Bean
+	public Config hazelcastConfig() {
+		MapConfig mapConfig = new MapConfig("spring-boot-admin-event-store").setInMemoryFormat(InMemoryFormat.OBJECT)
+				.setBackupCount(1).setEvictionPolicy(EvictionPolicy.NONE);
+		return new Config().setProperty("hazelcast.jmx", "true").addMapConfig(mapConfig);
 	}
+
+
+
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
 }
